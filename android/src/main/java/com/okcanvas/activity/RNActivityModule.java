e13f59100f9c751294f696d971c0d79cd22bc47f
okcanvas <okcanvas@naver.com>
2023-03-19 20:32:26 +0900

    first

--- android/src/main/java/com/okcanvas/activity/RNActivityModule.java
+++ android/src/main/java/com/okcanvas/activity/RNActivityModule.java
@@ -50,7 +50,6 @@ public class RNActivityModule extends ReactContextBaseJavaModule {
     Activity activity = getCurrentActivity();
     if (activity != null) {
       Intent intent = new Intent(activity, MiniAppActivity.class);
-      //Intent intent = new Intent(activity, Class.forName("com.okcanvas.activity.MiniAppActivity"));
       activity.startActivity(intent);
     }
   }
