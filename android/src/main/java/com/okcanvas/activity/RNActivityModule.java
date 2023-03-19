5e71d254885b78b51bfefc2fa3a2f742969d5719
okcanvas <okcanvas@naver.com>
2023-03-19 17:08:02 +0900

    first

---
+++ android/src/main/java/com/okcanvas/activity/RNActivityModule.java
@@ -0,0 +1,80 @@
+
+package com.okcanvas.activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.provider.MediaStore;
+import android.util.Base64;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.WritableMap;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class RNActivityModule extends ReactContextBaseJavaModule {
+
+  private final ReactApplicationContext reactContext;
+
+  public RNActivityModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+    this.reactContext = reactContext;
+  }
+
+  @Override
+  public String getName() {
+    return "RNActivityManager";
+  }
+
+  @ReactMethod
+  void navigateToExample() {
+    Activity activity = getCurrentActivity();
+    if (activity != null) {
+      Intent intent = new Intent(activity, MiniAppActivity.class);
+      activity.startActivity(intent);
+    }
+  }
+
+  @ReactMethod
+  void dialNumber(@NonNull String number) {
+    Activity activity = getCurrentActivity();
+    if (activity != null) {
+      Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + number));
+      activity.startActivity(intent);
+    }
+  }
+
+  @ReactMethod
+  void getActivityName(@NonNull Callback callback) {
+    Activity activity = getCurrentActivity();
+    if (activity != null) {
+      callback.invoke(activity.getClass().getSimpleName());
+    } else {
+      callback.invoke("No current activity");
+    }
+  }
+
+  @ReactMethod
+  void getActivityNameAsPromise(@NonNull Promise promise) {
+    Activity activity = getCurrentActivity();
+    if (activity != null) {
+      promise.resolve(activity.getClass().getSimpleName());
+    } else {
+      promise.reject("NO_ACTIVITY", "No current activity");
+    }
+  }
+
+}
---
+++ react-native.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dependency: {
+        platforms: {
+            android: {
+                packageImportPath: 'import com.okcanvas.activity.RNActivityPackage;',
+                packageInstance: 'new RNActivityPackage()',
+            },
+        },
+    },
+};
